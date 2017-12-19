edf651474d421d15301f0ce7de2544bb6c3d4503
Bernd Lienau <swim_bernd@gmx.net>
2017-12-19 01:49:53 +0100

    y tho

--- PythonGitter/PythonApplication1/PythonApplication1.py
+++ PythonGitter/PythonApplication1/PythonApplication1.py
@@ -256,7 +256,7 @@ def spalt(n,a,d,h,wl,zs,lowerrange,upperrange):
 		for intx in np.arange(-0.0015, 0.0015, 0.000005):
 			arrayX.append(Transmission_n_Spalte(intx,2,a,d))
 		plt.plot(np.arange(-0.0015, 0.0015, 0.000005),np.array(arrayX), 'r--')
-		plt.xlim(-0.05, 0.05)
+		plt.xlim(-0.0015, 0.0015)
 		plt.ylim(-1,2)
 		##change end
 		
